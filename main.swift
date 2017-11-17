8762fe73540d2d25af7eab28d4ed395ecc0bdb4c
ae.ivlev <ae.ivlev@tensor.ru>
2017-11-17 16:53:25 +0700

    fix command line.

--- main.swift
+++ main.swift
@@ -1,9 +1,9 @@
 c_main() // initialization C data
 
 let runner: Runner
-if 3 == CommandLine.arguments.count {
+if 4 == CommandLine.arguments.count {
   let args = CommandLine.arguments
-  runner = Runner(host: args[0], port: args[1], token: args[2])
+  runner = Runner(host: args[1], port: args[2], token: args[3])
 } else {
   runner = Runner(host: "127.0.0.1", port: "31001", token: "0000000000000000")
 }
